050e9d153bba7e2ca61ec18d4446e5713d10a4a9
LeeSubin3020 <97301965+LeeSubin3020@users.noreply.github.com>
2022-10-14 19:01:10 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+ 
   <nav>
     <RouterLink to="/">Main</RouterLink>
     <RouterLink to="/list">List</RouterLink>
+    <RouterLink to="/search">Search</RouterLink>
+   
   </nav>
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style scoped>
