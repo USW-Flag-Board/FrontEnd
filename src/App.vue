d2a6d48c01d2cd1fff11fbaed4a2a3e6550c9aa1
KimJunpyo <bono1725@naver.com>
2022-11-01 08:42:23 +0900

    1
    123

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterLink to="/SignUpPage">SignUp</RouterLink>
   </nav>
   <RouterView />
+  <RouterView />
 </template>
 
 <style scoped>
