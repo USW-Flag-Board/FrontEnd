99c95c4acf58bbe5dcd1c7d0fe31c69417075174
KimJunpyo <bono1725@naver.com>
2022-11-16 20:11:16 +0900

    React Base Up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./component/Header";
+import LoginPage from "./component/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
