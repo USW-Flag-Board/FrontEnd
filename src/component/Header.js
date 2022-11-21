d6e61570d5607a400bde83d196663978253750e0
Eojoonhyuk <94384392+Eojoonhyuk@users.noreply.github.com>
2022-11-21 18:55:14 +0900

    feat: home 레이아웃 수정

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,58 +5,82 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import { Container } from "@mui/material";
 import { styled } from '@mui/system';
 
 
 const HomeHeader = styled('div')({
-  backgroundColor: 'black',
   width: "100vw",
-  height: "10vh"
+  height: "10vh",
+  display: "flex",
+  alignItems: "flex-end",
+});
+
+const LogoBox = styled('div')({
+  width: "20vw",
+  height: "100%",
+  display: "flex", 
+  alignItems: "center",
+  paddingLeft: "15px"
+});
+
+const MenuItemBox = styled('div')({
+  width: "80vw",
+  display: "flex",
+  justifyContent: "end",
+  alignItems: "center",
+  marginRight: "5px",
 })
+
+const MenuItems = styled('ul')({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-end",
+  margin: 0,
+});
+
+const MenuItem = styled('li')({
+  width: "200px",
+  height: "50px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  '&:hover':{
+      backgroundColor: "#adb5bd", 
+      borderRadius: "10px"
+  }
+});
 
 const Header = () => {
   const sections = ["FLAG", "BOARD", "ACTIBITY", "NOTICE"];
   return (
     <Box>
-      <HomeHeader></HomeHeader>
+      <HomeHeader>
+        <LogoBox>
+          <img className="logo" src="img/logo.JPG" alt="blog-logo" style={{height: "80%"}}/>
+        </LogoBox>
+        <Box>
+          <MenuItemBox>
+            <MenuItems>
+              {sections.map((item) => <MenuItem key={item}>{item}</MenuItem>)}
+            </MenuItems>
+            <Paper 
+              component="form"
+              sx={{display: 'flex', width: 250, height: 30, borderRadius: "15px", ml: 2, border: "1px solid #adb5bd"}}
+              >
+                <IconButton type="button" sx={{ p: '10px'}} aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+                <InputBase
+                  sx={{ ml: 1, flex: 1}}
+                />
+              </Paper>
+              <IconButton sx={{ p: '8px'}} aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+          </MenuItemBox>
+        </Box>
+      </HomeHeader>
     </Box>
-    // <Container sx={{width: "100vw", height: "100vh"}}>
-    //   <Box>
-    //     <img className="logo" src="img/logo.JPG" alt="blog-logo" style={{height: "100%"}}/>
-    //   </Box>
-    // </Container>  
-
-            // <Box sx={{display: 'flex', alignItems: 'center', }}>
-            //   {sections.map((section) => (
-            //     <Box
-            //       key={section}
-            //       variant="body1"
-            //       sx={{ display: 'flex', alignItems: 'center', flexShrink: 0, width: 175, height: 60, justifyContent: 'center'
-            //     ,"&:hover":{
-            //       backgroundColor: "#adb5bd"
-            //       , borderRadius: "10px"
-            //     }}}
-            //     >
-            //       {section}
-            //     </Box>
-            //   ))}
-            //   <Paper 
-            //   component="form"
-            //   sx={{display: 'flex', width: 200, height: 30, borderRadius: "15px", ml: 2}}
-            //   >
-            //     <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-            //       <SearchIcon />
-            //     </IconButton>
-            //     <InputBase
-            //       sx={{ ml: 1, flex: 1}}
-            //     />
-            //   </Paper>
-            //   <IconButton sx={{ p: '8px', display: 'flex', alignItems: 'flex-end'}} aria-label="menu">
-            //     <MenuIcon />
-            //   </IconButton>
-            // </Box>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,168 @@
 import React from 'react'
+import { styled } from '@mui/system';
 import Header from '../component/Header';
 
+const HomeArea = styled('div')({
+    backgroundColor: "#212529",
+    height: "90vh",
+    display: "flex",
+});
+
+const MyActivity = styled('div')({
+    width: "50%",
+    height: "100%",
+    paddingLeft: "30px"
+});
+
+const FlagContents = styled('ul')({
+    display: "flex",
+    listStyle:"none",
+    width: "100%",
+    marginBottom: "2px",
+});
+
+const FlagContent = styled('li')({
+    display: "flex",
+    justifyContent: "center",
+    width: "25%",
+    padding: "1px",
+});
+
+const BookImgBox = styled('div')({
+    height: "45%",
+});
+
+const GrassArea = styled('div')({
+    width: "95%",
+    height: "25%",
+    marginTop: "10px",
+    backgroundColor: "black",
+    display: 'flex'
+});
+
+const GrassBox = styled('div')({
+    width: "33.3%",
+    margin: "5px",
+    backgroundColor: "white"
+});
+
+const FeedArea = styled('div')({
+    width: "30%",
+});
+
+const FeedBox = styled('div')({
+    width: "90%",
+    height: "60%",
+    marginTop: "30px",
+});
+
+const FeedItem = styled('div')({
+    backgroundColor: "white",
+    margin: "10px",
+    height: "30%",
+    borderRadius: "30px"
+});
+
+const NoticeArea = styled('div')({
+    display: "flex",
+    height: "20%",
+    width: "90%",
+});
+
+const NoticeBox = styled('div')({
+    width: "50%",
+    margin: "10px",
+});
+
+const UserBox = styled('div')({
+    width: "50%",
+    margin: "10px",
+});
+
+const SideArea = styled('div')({
+    width: "20%",
+    height: "100%",
+});
+
+const ButtonArea = styled('div')({
+    display: "flex",
+    alignItems: 'center',
+    justifyContent: "flex-end",
+    height: "11%",
+});
+
+const ButtonItem = styled('div')({
+    width: "12%",
+    height: "40%", 
+    margin: "5px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "20px",
+    borderRadius: "12px"
+});
+
+const SideBar = styled('div')({
+    backgroundColor: "#868e96",
+    height: "89%",
+    width: "100%",
+});
+
 const Home = () => {
-    return (
+    const contents = [
+        {title: "STUDY", content: ["ALGORITHM", "WEB-BACKEND"]}, 
+        {title: "PROJECT", content: ["FLAG-게시판"]}
+    ];
+    
+    const buttonItem = [
+        {name: "CH", bgColor: "#5c940d", fontColor: "white"}, 
+        {name: "N" , bgColor: "#f8f9fa", fontColor: "black"},
+        {name: "G", bgColor: "#9775fa", fontColor: "white"},
+    ];
+        return (
         <>
             <Header/>
+            <HomeArea>
+                <MyActivity>
+                    <h3 style={{color: "white", paddingTop: "30px", margin: 0}}>MY ACTIBITY</h3>
+                    <FlagContents>
+                        {contents.map((item)=>(<FlagContent key={item.title}><h4 style={{color: "white"}}>{item.title}</h4></FlagContent>))}
+                    </FlagContents>
+                    <BookImgBox>
+                        <img src="img/home-book.jpg" alt="blog-logo" style={{width: "95%",height: "100%"}}/>
+                    </BookImgBox>
+                    <h3 style={{color: "white", paddingTop: "30px", margin: 0}}>STUDY_WEB-BACKEND</h3>
+                    <GrassArea>
+                        <GrassBox/>
+                        <GrassBox/>
+                        <GrassBox/>
+                    </GrassArea>
+                </MyActivity>
+                <FeedArea>
+                    <h3 style={{color: "white", paddingTop: "30px", margin: 0}}>FEED</h3>
+                    <FeedBox>
+                        <FeedItem/>
+                        <FeedItem/>
+                        <FeedItem/>
+                    </FeedBox>
+                    <NoticeArea>
+                        <NoticeBox>
+                            <h4 style={{color: "white", margin: 0, paddingBottom: "5px"}}>#NOTICE</h4>
+                            <p style={{color: "white"}}>할말은 없지만 공지입니다.</p>
+                        </NoticeBox>
+                        <UserBox>
+                            <h4 style={{color: "white", margin: 0, paddingBottom: "5px"}}>USER</h4>
+                            <p style={{color: "white"}}>강지은, 김준표, 문희조, 이수빈, 어준혁, 임소미, 한지석</p>
+                        </UserBox>
+                    </NoticeArea>
+                </FeedArea>
+                <SideArea>
+                    <ButtonArea>
+                        {buttonItem.map((item)=>(<ButtonItem key={item.name} style={{color: `${item.fontColor}`, backgroundColor: `${item.bgColor}` }}>{item.name}</ButtonItem>))}
+                    </ButtonArea>
+                    <SideBar></SideBar>
+                </SideArea>
+            </HomeArea>
         </>
     )
 }
