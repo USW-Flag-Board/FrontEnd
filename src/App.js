f1409f13e4fa87219f07a0ba9ec8239e3bd61287
KimJunpyo <bono1725@naver.com>
2022-12-12 20:57:12 +0900

    refactor: LoginPage and App.js
    1. LoginPage 리팩토링 버전 파일로 업그레이드
    2. App.js router 미지정 페이지 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import BulletinBoard from "./pages/BulletinBoard";
 import Header from "./component/Header";
 import WritePost from "./pages/WritePost";
+import MyPage from "./pages/MyPage";
+import WriteDetail from "./pages/DetailWritePage";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
@@ -28,23 +30,24 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <GlobalStyle/>
-        <Header/>
+        <GlobalStyle />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/board" element={<BulletinBoard/>}></Route>
+          <Route path="/my" element={<MyPage />}></Route>
+          <Route path="/board/writeDetail" element={<WriteDetail />}></Route>
+          <Route path="/board" element={<BulletinBoard />}></Route>
           <Route path="/board/write" element={<WritePost />}></Route>
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
